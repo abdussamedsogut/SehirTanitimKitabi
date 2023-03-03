342f2f9cd979db112e7f336755e9c00c187bdc58
Asamed <asamedsogut@hotmail.com>
2023-03-03 07:51:40 +0300

    Initial Commit

---
+++ SehirTanitimKitabi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SehirTanitimKitabi
+//
+//  Created by Abdüssamed Söğüt on 3.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
